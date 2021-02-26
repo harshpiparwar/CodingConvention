cec4e01bedb85429cced1e850054a98bf6e27ef9
harshpiparwar <76031070+harshpiparwar@users.noreply.github.com>
2021-02-26 17:34:34 +0530

    Updated File

---
+++ Employee1.java
@@ -0,0 +1,28 @@
+public class Employee1 {
+    int id;
+    double salary = 0.0;
+
+    public Employee1(int id) {
+        this.id = id;
+    }
+
+    public Employee1() {
+    }
+
+    double calTax() {
+        if (this.salary > 500000.00 && this.salary < 1000000)
+            return this.salary * 0.1;
+
+        else if (this.salary > 1000000.00 && this.salary < 1500000)
+            return this.salary * 0.2;
+
+        else
+            return this.salary * 0.3;
+
+
+        for(int index = 0; index < 10; index++){
+
+        }
+
+    }
+}
